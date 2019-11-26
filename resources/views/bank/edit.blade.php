2a0603215158674259f6d305e27ebb977a02e01e
guanxiaoke123 <1773138266@qq.com>
2019-11-26 10:50:39 +0800

    修改银行卡效验

--- resources/views/bank/edit.blade.php
+++ resources/views/bank/edit.blade.php
@@ -17,7 +17,7 @@
     <div class="layui-form-item">
         <label class="layui-form-label">开户行：</label>
         <div class="layui-input-block">
-            <input type="text" value="{{$info['deposit_name'] or ''}}" id="bank_name" name="deposit_name" required lay-verify="deposit_name" placeholder="请输入2-12位汉字" autocomplete="off" class="layui-input">
+            <input type="text" value="{{$info['deposit_name'] or ''}}" id="bank_name" name="deposit_name" required readonly="readonly" lay-verify="deposit_name" placeholder="此框为自动填充并且不能为空！" autocomplete="off" class="layui-input">
         </div>
     </div>
 @endsection
@@ -32,6 +32,18 @@
             var layer = layui.layer;
             form.verify({
                 name: [/[\u4e00-\u9fa5]{2,30}$/, '请输入正确的姓名'],
+                deposit_card: function (value) {
+                    if(value==null||value==''){
+                        return "银行卡号不能为空！";
+                    }else if(value.length<14 && value.length>19){
+                        return "银行卡格式有误！";
+                    }
+                },
+                deposit_name:function (value) {
+                    if(value==null || value==''){
+                        return "开户行不能为空";
+                    }
+                }
             });
             $("#bank_idcard").blur(function(){
                 var value=$(this).val();
@@ -74,6 +86,63 @@
                 return false;
             });
         });
+        //银行卡正则校验
+        function checkBankno(bankno) {
+            var lastNum = bankno.substr(bankno.length - 1, 1);//取出最后一位（与luhm进行比较）
+            var first15Num = bankno.substr(0, bankno.length - 1);//前15或18位
+            var newArr = [];
+
+            for (var i = first15Num.length - 1; i > -1; i--) { //前15或18位倒序存进数组
+                newArr.push(first15Num.substr(i, 1));
+            }
+
+            var arrJiShu = []; //奇数位*2的积 <9
+            var arrJiShu2 = []; //奇数位*2的积 >9
+            var arrOuShu = []; //偶数位数组
+            for (var j = 0; j < newArr.length; j++) {
+                if ((j + 1) % 2 == 1) {//奇数位
+                    if (parseInt(newArr[j]) * 2 < 9)
+                        arrJiShu.push(parseInt(newArr[j]) * 2); else
+                        arrJiShu2.push(parseInt(newArr[j]) * 2);
+                }
+                else //偶数位
+                    arrOuShu.push(newArr[j]);
+            }
+
+            var jishu_child1 = [];//奇数位*2 >9 的分割之后的数组个位数
+            var jishu_child2 = [];//奇数位*2 >9 的分割之后的数组十位数
+            for (var h = 0; h < arrJiShu2.length; h++) {
+                jishu_child1.push(parseInt(arrJiShu2[h]) % 10);
+                jishu_child2.push(parseInt(arrJiShu2[h]) / 10);
+            }
+
+            var sumJiShu = 0; //奇数位*2 < 9 的数组之和
+            var sumOuShu = 0; //偶数位数组之和
+            var sumJiShuChild1 = 0; //奇数位*2 >9 的分割之后的数组个位数之和
+            var sumJiShuChild2 = 0; //奇数位*2 >9 的分割之后的数组十位数之和
+            var sumTotal = 0;
+            for (var m = 0; m < arrJiShu.length; m++) {
+                sumJiShu = sumJiShu + parseInt(arrJiShu[m]);
+            }
+            for (var n = 0; n < arrOuShu.length; n++) {
+                sumOuShu = sumOuShu + parseInt(arrOuShu[n]);
+            }
+            for (var p = 0; p < jishu_child1.length; p++) {
+                sumJiShuChild1 = sumJiShuChild1 + parseInt(jishu_child1[p]);
+                sumJiShuChild2 = sumJiShuChild2 + parseInt(jishu_child2[p]);
+            }
+            //计算总和
+            sumTotal = parseInt(sumJiShu) + parseInt(sumOuShu) + parseInt(sumJiShuChild1) + parseInt(sumJiShuChild2);
+            //计算Luhm值
+            var k = parseInt(sumTotal) % 10 == 0 ? 10 : parseInt(sumTotal) % 10;
+            var luhm = 10 - k;
+            if (lastNum == luhm) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     </script>
 @endsection
 @extends('common.edit')
